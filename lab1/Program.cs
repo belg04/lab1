44e2441b378d4217c4b714d7f00856c0c6d57a48
glebe <glebe@BELG04>
2022-09-16 18:22:38 +0200

    Добавьте файлы проекта.

---
+++ lab1/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+    Console.WriteLine("Ведите число p:");
+    double p = double.Parse(Console.ReadLine());
+
+    Console.WriteLine("Ведите число y:");
+    double y = double.Parse(Console.ReadLine());
+
+    Console.WriteLine("Ведите число e:");
+    double e = double.Parse(Console.ReadLine());
+
+    double K = Math.Log(Math.Pow(p, 2) + (Math.Pow(y, 3))) + Math.Pow(e, p);
+    Console.WriteLine($"K={K:F2}");
